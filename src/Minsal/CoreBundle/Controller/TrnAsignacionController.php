acbdd06c975b8adc49d0160b90c5c45a1cf653d8
Kevin Rivera <kevinrev26@gmail.com>
2017-07-24 05:19:24 -0600

    Vista listar asignaciones

---
+++ src/Minsal/CoreBundle/Controller/TrnAsignacionController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Minsal\CoreBundle\Controller;
+
+use Minsal\CoreBundle\Entity\TrnAsignacion;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Trnasignacion controller.
+ *
+ */
+class TrnAsignacionController extends Controller
+{
+    /**
+     * Lists all trnAsignacion entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $trnAsignacions = $em->getRepository('MinsalCoreBundle:TrnAsignacion')->findAll();
+
+        return $this->render('trnasignacion/index.html.twig', array(
+            'trnAsignacions' => $trnAsignacions,
+        ));
+    }
+
+    /**
+     * Creates a new trnAsignacion entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $trnAsignacion = new Trnasignacion();
+        $form = $this->createForm('Minsal\CoreBundle\Form\TrnAsignacionType', $trnAsignacion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($trnAsignacion);
+            $em->flush();
+
+            return $this->redirectToRoute('asingaciones_show', array('id' => $trnAsignacion->getId()));
+        }
+
+        return $this->render('trnasignacion/new.html.twig', array(
+            'trnAsignacion' => $trnAsignacion,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a trnAsignacion entity.
+     *
+     */
+    public function showAction(TrnAsignacion $trnAsignacion)
+    {
+        $deleteForm = $this->createDeleteForm($trnAsignacion);
+
+        return $this->render('trnasignacion/show.html.twig', array(
+            'trnAsignacion' => $trnAsignacion,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing trnAsignacion entity.
+     *
+     */
+    public function editAction(Request $request, TrnAsignacion $trnAsignacion)
+    {
+        $deleteForm = $this->createDeleteForm($trnAsignacion);
+        $editForm = $this->createForm('Minsal\CoreBundle\Form\TrnAsignacionType', $trnAsignacion);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('asingaciones_edit', array('id' => $trnAsignacion->getId()));
+        }
+
+        return $this->render('trnasignacion/edit.html.twig', array(
+            'trnAsignacion' => $trnAsignacion,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a trnAsignacion entity.
+     *
+     */
+    public function deleteAction(Request $request, TrnAsignacion $trnAsignacion)
+    {
+        $form = $this->createDeleteForm($trnAsignacion);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($trnAsignacion);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('asingaciones_index');
+    }
+
+    /**
+     * Creates a form to delete a trnAsignacion entity.
+     *
+     * @param TrnAsignacion $trnAsignacion The trnAsignacion entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(TrnAsignacion $trnAsignacion)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('asingaciones_delete', array('id' => $trnAsignacion->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Minsal/CoreBundle/Form/TrnAsignacionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Minsal\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrnAsignacionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('apiAlmacenid')->add('descripcion')->add('fechadistribucion')->add('fechacorte')->add('mesesCpm')->add('mesesDistribucion')->add('mesesAdministracion')->add('mesesSeguridad')->add('fechaCreacion')->add('fechaModificaion')->add('prioridad')->add('segUsuarioid')->add('catEstadosid')->add('catProgramaid')->add('catSuministroid')->add('apiGruposuministroid')->add('idCatEstablecimiento');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Minsal\CoreBundle\Entity\TrnAsignacion'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'minsal_corebundle_trnasignacion';
+    }
+
+
+}
